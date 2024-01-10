4908629d235996a1cff56279e6be6f45c10bce3e
Eliaser <eperyaq@g.educaand.es>
2024-01-10 11:32:46 +0100

    Programa terminado pero con el error de pedirNum()

--- src/Main.kt
+++ src/Main.kt
@@ -52,23 +52,78 @@ fun pregunta(text:String): Boolean {
     }
 }
 
+/**
+ * Crea la tabla de multiplicar del numero que se haya introducido
+ *
+ * @param numero Int - Numero introducido por el usuario
+ *
+ *
+ */
+fun creacionTabla(numero:Int){
 
-fun creacionTabla(){
+    val multiplicadores = Array(10, { i -> i + 1 })
+    for (i in multiplicadores){
+        val resultado = i * numero
+        println("$i -> $numero x $i = $resultado")
 
+    }
 }
 fun main() {
     //TODO: Solicitar la introducción de un número entre 1 y 100 y mostrar su tabla de multiplicar...
     //Hasta que se responda negativamente a la pregunta "¿Desea generar otra tabla de multiplicación? (s/n)"
-    val min = 1
-    val max = 100
-    val numero = pedirNum(min, max)
-    println("La tabla de multiplicar del numero $numero es: " )
-    //Primero va introducir el numero luego la tabla es... y despues pregunta de nuevo
-    /*
-    val text:String = "¿Desea generar otra tabla de multiplicar? (s/n)"
+    var todoOk = true
+    while (todoOk) {
+        val min = 1
+        val max = 100
+        val numero = pedirNum(min, max)
 
-    pregunta(text)
-    */
+        println("La tabla de multiplicar del numero $numero es: ")
+        creacionTabla(numero)
 
+        //Primero va introducir el numero luego la tabla es... y despues pregunta de nuevo
 
+        val text: String = "¿Desea generar otra tabla de multiplicar? (s/n)"
+
+        val respuesta = pregunta(text)
+
+        if (respuesta == false){
+            todoOk=false
+        }
+    }
 }
+
+
+
+
+    /*do {
+        val min = 1
+        val max = 100
+        val numero = pedirNum(min, max)
+
+        println("La tabla de multiplicar del numero $numero es: " )
+        creacionTabla(numero)
+
+        //Primero va introducir el numero luego la tabla es... y despues pregunta de nuevo
+
+        val text:String = "¿Desea generar otra tabla de multiplicar? (s/n)"
+
+        val respuesta = pregunta(text)
+    }while (respuesta == true){
+        val min = 1
+        val max = 100
+        val numero = pedirNum(min, max)
+
+        println("La tabla de multiplicar del numero $numero es: " )
+        creacionTabla(numero)
+
+        //Primero va introducir el numero luego la tabla es... y despues pregunta de nuevo
+
+        val text:String = "¿Desea generar otra tabla de multiplicar? (s/n)"
+
+        val respuesta = pregunta(text)
+    }*/
+
+
+
+
+
